3dc2f37a17464d255bd40ab025f6ea30ea94448a
edsonnaza <edsonnaza@gmail.com>
2023-11-07 20:25:27 -0300

    ui-layout-pages component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
- 
-
 import App from './App.vue'
 import router from './router';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
 
 const app = createApp(App);
 app.use(router);
+app.component('base-spinner', BaseSpinner);
+app.component('base-card',BaseCard);
+app.component('base-button',BaseButton);
+app.component('base-dialog',BaseDialog);
+app.component('base-badge',BaseBadge);
 app.mount('#app');
